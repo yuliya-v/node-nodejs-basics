6e5035d97cc2fff433c4fb9466dfb8684cd70d1e
Yuliya Volchek <yuliavolchek@gmail.com>
2022-06-05 19:27:31 +0300

    feat: solve worker treads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,33 @@
+import os from 'os';
+import path from 'path';
+import { Worker } from 'worker_threads';
+import { getPath } from '../utils.js';
+
 export const performCalculations = async () => {
-    // Write your code here
+  const numOfCpus = os.cpus().length;
+  const { __dirname } = getPath(import.meta.url);
+  const pathToWorker = path.join(__dirname, 'worker.js');
+  const startNum = 10;
+
+  const workers = Array(numOfCpus)
+    .fill('')
+    .map((_, ind) => ind + startNum)
+    .map((val) => {
+      return new Promise((res, rej) => {
+        const worker = new Worker(pathToWorker, { workerData: val });
+        worker.on('message', res);
+        worker.on('error', rej);
+      })
+    })
+
+  Promise.allSettled(workers)
+    .then((data) => data
+      .map((obj) => ({
+        status: obj.status === 'fulfilled' ? 'resolved' : 'error',
+        data: obj.status === 'fulfilled' ? obj.value : null
+      }))
+    )
+    .then((data) => console.log(data))
 };
+
+performCalculations();
