c0a7b9df77a268e4ba4eabc8562299a595787e39
Yuliya Volchek <yuliavolchek@gmail.com>
2022-06-05 17:08:59 +0300

    feat: solve streams task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,25 @@
+import process from 'process';
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { stdin, stdout } from 'process';
+import { getPath } from '../utils.js';
+
 export const write = async () => {
-    // Write your code here 
+  const { __dirname } = getPath(import.meta.url);
+  const pathToFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+  
+  const output = createWriteStream(pathToFile);
+
+  stdout.write('Введите ваш текст:\n');
+
+  stdin.on('data', (data) => {
+    output.write(data);
+  })
+
+  process.on('SIGINT', () => {
+    stdout.write('\nТекст сохранён в файле "fileToWrite.txt".\n');
+    process.exit();
+});
 };
+
+write();
