503a195cf0d7cfcfbb8620437188054660e48227
Yuliya Volchek <yuliavolchek@gmail.com>
2022-06-05 05:25:51 +0300

    feat: solve cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2)
+    .reduce((acc, val, ind, arr) => {
+      if (!(ind % 2)) {
+        acc[val.slice(2)] = arr[ind + 1];
+      }
+      return acc;
+    }, {});
+  
+  return Object.entries(args)
+   .map(([key, val]) => `${key} is ${val}`)
+   .join(', ');
+
 };
+
+console.log(parseArgs());
