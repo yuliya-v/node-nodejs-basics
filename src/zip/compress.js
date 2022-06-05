86647aa1c679c25d0815672b0792a694d6570abf
Yuliya Volchek <yuliavolchek@gmail.com>
2022-06-05 17:38:24 +0300

    feat: solve zlib task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,19 @@
+import { getPath } from "../utils.js";
+import path from 'path';
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+
+
 export const compress = async () => {
-    // Write your code here 
+  const { __dirname } = getPath(import.meta.url);
+  const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const pathToOutput = path.join(__dirname, 'files', 'archive.gz');
+  const encoding = 'utf-8';
+  const readStream = createReadStream(pathToFile, encoding);
+  const writeStream = createWriteStream(pathToOutput);
+  const gzip = createGzip();
+
+  readStream.pipe(gzip).pipe(writeStream);
 };
+
+compress();
