21027e033774b6c3b0ccc148d1960534845fa523
Yuliya Volchek <yuliavolchek@gmail.com>
2022-06-05 21:04:56 +0300

    feat: solve child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,16 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { stdin } from 'process';
+import { getPath } from '../utils.js';
+import process from 'process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const { __dirname } = getPath(import.meta.url);
+  const pathToScript = path.join(__dirname, 'files', 'script.js');
+  const child = spawn('node', [ pathToScript, ...args]);
+
+  child.stdout.pipe(process.stdout);
+  stdin.pipe(child.stdin);
 };
+
+spawnChildProcess(['аргумент', 'аргументик']);
